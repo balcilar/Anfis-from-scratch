853e25e2561c3ad54d7429246d7d526d25ec4094
Muhammet Balcilar <muhammetbalcilar@gmail.com>
2018-09-18 16:55:24 +0200

    Update demo1.m

--- demo1.m
+++ demo1.m
@@ -18,6 +18,24 @@ data=load('Input/data3input.trn');
 % run our anfis model
 [bestnet,y_myanfis,RMSE]=myanfis(data,epoch_n,mf,step_size,decrease_rate,increase_rate);
 
+figure;imagesc(bestnet.config)
+title('Node Connections');
+
+figure;
+for i=1:3
+    subplot(1,3,i);
+    x = -10:0.1:10;%min(data(:,i))-1:0.1:max(data(:,i))-1;
+    y = gbellmf(x,bestnet.mparams((i-1)*2+1,:));plot(x,y)
+    hold on;
+    y = gbellmf(x,bestnet.mparams((i-1)*2+2,:));plot(x,y,'r-')
+    msg=[num2str(i) 'th variables membership functions'];
+    title(msg);
+end
+
+
+% make prediction
+y_myanfis=evalmyanfis(bestnet,data(:,1:end-1));
+
 % plot error decreasing
 figure;plot(RMSE,'LineWidth',2);xlabel('iteration');
 ylabel('rmse');title('Error by iteration');
@@ -36,18 +54,3 @@ rmse1=sqrt(sum((y_myanfis-data(:,end)).^2)/size(data,1));
 
 msg=['Total rmse error myanfis:' num2str(rmse1)];
 title(msg);
-
-
-
-
-
-
-
-
-
-     
-
-
-
-
-
